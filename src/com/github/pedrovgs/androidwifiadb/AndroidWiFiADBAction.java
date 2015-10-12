1e745ad08a96d60ae14b24aac3018e6e648c700a
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-10-12 15:10:49 +0200

    Move application to the correct package

---
+++ src/com/github/pedrovgs/androidwifiadb/AndroidWiFiADBAction.java
@@ -0,0 +1,15 @@
+package com.github.pedrovgs.androidwifiadb;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+
+public class AndroidWiFiADBAction extends AnAction {
+
+  public void actionPerformed(AnActionEvent event) {
+    
+  }
+
+}
