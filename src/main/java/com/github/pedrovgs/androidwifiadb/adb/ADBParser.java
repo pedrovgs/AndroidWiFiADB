4869ffe171bca41c46d682b4408a2f477db4c221
pedrovgs <pedrovicente.gomez@gmail.com>
2015-10-18 19:05:39 +0200

    Fix bug related to the getDevices parsing when there are no devices connected

--- src/main/java/com/github/pedrovgs/androidwifiadb/adb/ADBParser.java
+++ src/main/java/com/github/pedrovgs/androidwifiadb/adb/ADBParser.java
@@ -8,14 +8,20 @@ public class ADBParser {
 
   private static final String MODEL_INDICATOR = "model:";
   private static final String DEVICE_INDICATOR = "device:";
+  private static final String IP_SEPARATOR = ".";
+  private static final String END_DEVICE_IP_INDICATOR = "/";
+  private static final String START_DEVICE_IP_INDICATOR = "t";
 
   public List<Device> parseGetDevicesOutput(String adbDevicesOutput) {
     List<Device> devices = new LinkedList<Device>();
     String[] splittedOutput = adbDevicesOutput.split("\\n");
+    if (splittedOutput.length == 1) {
+      return devices;
+    }
     for (String line : splittedOutput) {
       String[] deviceLine = line.split("\\t");
       String id = deviceLine[0].substring(0, deviceLine[0].indexOf(" "));
-      if (id.contains(".") || id.contains("List")) {
+      if (id.contains(IP_SEPARATOR)) {
         continue;
       }
       int start = line.indexOf(MODEL_INDICATOR) + MODEL_INDICATOR.length();
@@ -36,8 +42,8 @@ public class ADBParser {
     }
 
     String[] splittedOutput = ipInfoOutput.split("\\n");
-    int end = splittedOutput[1].indexOf("/");
-    int start = splittedOutput[1].indexOf("t");
+    int end = splittedOutput[1].indexOf(END_DEVICE_IP_INDICATOR);
+    int start = splittedOutput[1].indexOf(START_DEVICE_IP_INDICATOR);
     return splittedOutput[1].substring(start + 2, end);
   }
 }
