9d670336ab6c13827c204cdad679fda15f7e8aae
pedrovgs <pedrovicente.gomez@gmail.com>
2015-10-18 19:21:13 +0200

    Change how we execute tasks in a background thread to use part of the ItelliJ plugins api

--- src/main/java/com/github/pedrovgs/androidwifiadb/action/AndroidWiFiADBAction.java
+++ src/main/java/com/github/pedrovgs/androidwifiadb/action/AndroidWiFiADBAction.java
@@ -29,6 +29,10 @@ import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.project.Project;
 
 public class AndroidWiFiADBAction extends AnAction implements View {
 
@@ -46,11 +50,12 @@ public class AndroidWiFiADBAction extends AnAction implements View {
   }
 
   public void actionPerformed(AnActionEvent event) {
-    new Thread(new Runnable() {
-      @Override public void run() {
+    Project project = getEventProject(event);
+    ProgressManager.getInstance().run(new Task.Backgroundable(project, ANDROID_WIFI_ADB_TITLE) {
+      @Override public void run(ProgressIndicator progressIndicator) {
         androidWifiADB.connectDevices();
       }
-    }).start();
+    });
   }
 
   @Override public void showNoConnectedDevicesNotification() {
